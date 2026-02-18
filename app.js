986ce1a64c0224a6ba119b84b82857eef2a667be
Nikhil Kumar Jha <nikhiljha997@gmail.com>
2026-02-19 00:37:08 +0530

    Added index route and made ejs template to show listing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -15,22 +16,31 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res)=>{
     res.send("I am root");
 })
 
-app.get("/testListing", async (req, res)=>{
-    let sampleListing = new Listing({
-        title: "My Villa",
-        description: "by the beach",
-        price: 1200,
-        location: "Mumbai",
-        country: "India"
-    })
-    await sampleListing.save();
-    console.log("Sample saved");
-    res.send("Successfull testing")
+// Index route
+app.get("/listings", async (req, res)=>{
+    const allListings = await Listing.find({})
+    res.render("listings/index.ejs", {allListings})
 })
+
+// app.get("/testListing", async (req, res)=>{
+//     let sampleListing = new Listing({
+//         title: "My Villa",
+//         description: "by the beach",
+//         price: 1200,
+//         location: "Mumbai",
+//         country: "India"
+//     })
+//     await sampleListing.save();
+//     console.log("Sample saved");
+//     res.send("Successfull testing")
+// })
 
 app.listen(3000, ()=>{
     console.log("app is listening on port 3000");
